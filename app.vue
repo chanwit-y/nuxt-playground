bbab88ecf55412c1c10b79bee96e1c5bcf889ccd
cy <chanwit.yimneam@hotmail.com>
2024-11-09 09:39:20 +0700

    test create vue flow on nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+import Flow from './components/Flow.vue'
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ClientOnly fallback-tag="span" fallback="Loading comments...">
+    <Flow />
+  </ClientOnly>
 </template>
