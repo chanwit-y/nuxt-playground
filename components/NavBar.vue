35d4fe4e7205d7bd624eaa13f7a82986af1a0add
cy <chanwit.yimneam@hotmail.com>
2024-11-12 14:59:12 +0700

    feat: update NavBar component by removing unused icon and add @iconify/json dependency

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
 	<nav p4 text-lg border-b border-base>
-		<div i-logos-nuxt-icon />
+		<!-- <div i-logos-nuxt-icon /> -->
+		<div i-line-md-bell-filled />
 		learn.nuxt.com
 	</nav>
 </template>
